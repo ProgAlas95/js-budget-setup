0213cc0ed89dfad8b3f9ad64080555ca1b9ba50f
AJM95 <alimagoti95@gmail.com>
2019-11-26 02:12:31 +0300

    Budget-Calculator

--- js/app.js
+++ js/app.js
@@ -15,4 +15,133 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+  //submit budget method
+  submitBudgetForm(){
+    const value = this.budgetInput.value
+    if (value=== '' || value< 0){
+      this.budgetFeedback.classList.add('showItem')
+      this.budgetFeedback.innerHTML = `<p> value cant be empty or negative </p>`
+      const self = this
+      setTimeout(function () {
+        self.budgetFeedback.classList.remove('showItem')
+      },4000)
+    }
+    else {
+      this.budgetAmount.textContent = value
+      this.budgetInput.value = ''
+      this.showBalance()
+    }
+  }
+  //show balance
+  showBalance(){
+    const expense = this.totalExpense()
+    const total = parseInt(this.budgetAmount.textContent) - expense
+    this.balanceAmount.textContent = total
+    if (total<0){
+      this.balance.classList.remove('showGreen', 'showBlack')
+      this.balance.classList.add('showGreen')
+    }
+    else if (total>0){
+      this.balance.classList.remove('showRed', 'showBlack')
+      this.balance.classList.add('showGreen')
+    }
+    else if (total===0){
+      this.balance.classList.remove('showRed', 'showGreen')
+      this.balance.classList.add('showBlack')
+    }
+  }
+  submitExpenseForm(){
+    const  expensiveValue = this.expenseInput.value
+    const amoutValue = this.amountInput.value
+    if (expensiveValue === ''|| amoutValue === ''|| amoutValue< 0){
+      this.budgetFeedback.classList.add(('showItem'))
+      this.expenseFeedback.innerHTML =`<p> value cant be empty or negative </p>`
+      const self = this
+      setTimeout(function () {
+        self.budgetFeedback.classList.remove('showItem')
+      },4000)
+    }
+    else {
+      let amount = parseInt(amoutValue)
+      this.expenseInput.value = ''
+      this.amountInput.value = ''
+      let expense = {
+        id:this.itemID,
+        title:expensiveValue,
+        amount:amount,
+      }
+      this.itemID++
+      this.itemList.push(expense)
+      this.addExpense(expense)
+      this.showBalance()
+
+      //show balance
+    }
+  }
+
+  //add expense
+  addExpense(expense){
+    const div = document.createElement('div')
+    div.classList.add('expense')
+    div.innerHTML = `<div class="expenes">
+        <div class="expense-item d-flex justify-content-between align-items-baseline">
+
+         <h6 class="expense-title mb-0 text-uppercase list-item">${expense.title}</h6>
+         <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+         <div class="expense-icons list-item">
+
+          <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+           <i class="fas fa-edit"></i>
+          </a>
+          <a href="#" class="delete-icon" data-id="${expense.id}">
+           <i class="fas fa-trash"></i>
+          </a>
+         </div>
+        </div>`
+
+    this.expenseList.appendChild(div)
+  }
+
+  totalExpense(){
+    let total = 0
+    if (this.itemList.length>0){
+      total =this.itemList.reduce(function (acc, curr) {
+        acc+=curr.amount
+        return acc
+      },0)
+    }
+    this.expenseAmount.textContent = total
+    return total
+  }
 }
+function  eventListner() {
+  const budgetForm = document.getElementById('budget-form')
+  const expenseForm = document.getElementById('expense-form')
+  const expenseList = document.getElementById('expense-list')
+
+  //new instance of UI Class
+  const ui = new UI()
+
+  //budget form submit
+  budgetForm.addEventListener('submit', function () {
+    event.preventDefault()
+    ui.submitBudgetForm()
+      })
+  //expense  form submit
+  expenseForm.addEventListener('submit', function () {
+    event.preventDefault()
+    ui.submitExpenseForm()
+  })
+
+  //expense click form submit
+  expenseList.addEventListener('click', function () {
+
+  })
+
+
+}
+document.addEventListener('DOMContentLoaded', function () {
+  eventListner()
+
+})
